f7717dc11cfb77fc644cca90e5ec148865ef3bef
Tinilinsk <120578910+Tinilinsk@users.noreply.github.com>
2025-02-25 00:06:30 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Dota Stat</title>
+</head>
+<body class="container">
+    <header>
+        <div class="header_menu">
+            <div class="navigation_menu">
+                <div class="logo">
+                    <a href="index.php">
+                        <span>DOTASTAT</span>
+                    </a>    
+                </div>
+                <div class="navigation_item">
+                    <ul class="navigation_list" style="list-style-type: none;">
+                        <li>
+                        <a href="">Heroes</a>
+                        </li>
+                        <li>
+                        <a href="">Items</a>
+                        </li>
+                        <li><a href="">Players</a></li>
+                        <li>
+                        <a href="">Matches</a>
+                        </li>
+                        <li>
+                        <a href="">News</a>
+                        </li>
+                    </ul>    
+                </div>
+            </div>
+            <div class="small_menu">
+                <div class="search_icon">
+                    <a href="">
+                        <i class="fa fa-search"></i>
+                    </a>
+                </div>
+                <div class="registration">
+                    <button>Sing In</button>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="main">
+        <div class="content">
+            <div class="news_section">
+                <h2>Latest News</h2>
+                <div class="news_item">Placeholder for news 1</div>
+                <div class="news_item">Placeholder for news 2</div>
+                <div class="news_item">Placeholder for news 3</div>
+                <div class="news_item">Placeholder for news 4</div>
+            </div>
+            <div class="side_section">
+                <div class="top_players">
+                    <h2>Top Players</h2>
+                    <div class="player_item">Player 1</div>
+                    <div class="player_item">Player 2</div>
+                    <div class="player_item">Player 3</div>
+                </div>
+
+                <div class="top_heroes">
+                    <h2>Top Heroes</h2>
+                    <div class="hero_item">Hero 1</div>
+                    <div class="hero_item">Hero 2</div>
+                    <div class="hero_item">Hero 3</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
